5b9c42cdb157789927dcf708217642d305bd12c7
AyushWaghe <144901247+AyushWaghe@users.noreply.github.com>
2024-02-19 21:58:24 +0530

    Create facultyAuthController.js

---
+++ controllers/facultyAuthController.js
@@ -0,0 +1,53 @@
+import facultyModel from "../models/facultyModel.js";
+import JWT from 'jsonwebtoken';
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+
+//POST LOGIN
+export const facultyLoginController=async(req,res) =>{
+    try {
+       const {email,password} = req.body
+       //Validation
+       if(!email || !password){
+        return res.status(404).send({
+            success:false,
+            message:'Invalid Username or password'
+        })
+       }
+       //check user
+       const user = await facultyModel.findOne({email})
+       if(!user){
+        return res.status(404).send({
+            success:false,
+            message:'Email is not Registered'
+        })
+       }
+       const match= await comparePassword(password,user.password)
+       if(!match){
+        return res.status(404).send({
+            success:false,
+            message:'Password did not match'
+        })
+       }
+    
+       //token
+       const token = await JWT.sign({ _id: user.id},process.env.JWT_SECRET, {expiresIn:"900d"});
+    
+       res.status(200).send({
+        success:true,
+        message:'Logged In Succesfully',
+        user:{
+           email:user.email,
+           role:user.role
+        },
+        token,
+       })
+    
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'error in Login',
+            error
+        })
+    }
+    };
